bb61a5210d895b31b89a28b5cd8b55f1af19c354
RidvanOzturk <ozturkridvan1@gmail.com>
2023-06-24 14:06:14 +0300

    Adding new format
    Adding new format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,74 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
- 
-  var number1;
-  var number2;
-  var result;
-  function addition(){
-result=number1+number2;
-alert(result);
-}
 
+  var result;
+  const [firstOperation, setFirstOperation] = useState(null);
+  const [secondOperation, setSecondOperation] = useState(null);
+  
+function handleInput(el) {
+
+  if(firstOperation == null){
+    setFirstOperation(el.target.value)
+  }
+  else if(secondOperation == null){
+    setSecondOperation(el.target.value)
+  }
+  console.log("First: " + firstOperation + " | Second: " + secondOperation);
+}
+const handleAddition = () => {
+
+  let result = parseInt(firstOperation)+parseInt(secondOperation);
+  alert(result);
+  setFirstOperation(null);
+  setSecondOperation(null);
+}
+const handleSubtraction = () => {
+
+  let result = parseInt(firstOperation)-parseInt(secondOperation);
+  alert(result);
+  setFirstOperation(null);
+  setSecondOperation(null);
+}
+const handleDivision= () => {
+
+  let result = parseFloat(firstOperation)/parseFloat(secondOperation);
+  alert(result);
+  setFirstOperation(null);
+  setSecondOperation(null);
+}
+const handleMultiplication = () => {
+
+  let result = parseInt(firstOperation)*parseInt(secondOperation);
+  alert(result);
+  setFirstOperation(null);
+  setSecondOperation(null);
+}
   return (
     <>
       <div className='App'>
         <div className='Calculator'>
-          <button onClick={addition}>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>/</button><br></br>
-          <button onClick={number1=1}>{number1 ? number2 : number1 }1</button>
-          <button onClick={number2=2 || number1=2}>2</button>
-          <button onClick={number1=3}>3</button><br></br>
-          <button onClick={number1=4}>4</button>
-          <button>5</button>
-          <button>6</button><br></br>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button onClick={handleAddition}>+</button>
+          <button onClick={handleSubtraction}>-</button>
+          <button onClick={handleMultiplication}>*</button>
+          <button onClick={handleDivision}>/</button><br></br>
+       
+          <button onClick={handleInput} value="1">1</button>
+          <button onClick={handleInput} value="2">2</button>
+          <button onClick={handleInput} value="3">3</button><br></br>
+          <button onClick={handleInput} value="4">4</button>
+          <button onClick={handleInput} value="5">5</button>
+          <button onClick={handleInput} value="6">6</button><br></br>
+          <button onClick={handleInput} value="7">7</button>
+          <button onClick={handleInput} value="8">8</button>
+          <button onClick={handleInput} value="9">9</button><br></br>
+          <button onClick={handleInput} value=" ">C</button>
           
-        </div>
-      </div>
+
+          
+        </div></div>
     </>
   )
 }
